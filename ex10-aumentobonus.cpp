0925899e3f303bfdd9e96aec32a04df1856a3bde
Eduarda Gama <eduarda.gama@salg.com.br>
2018-09-03 15:56:27 -0300

    Create ex10-aumentobonus.cpp

---
+++ ex10-aumentobonus.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+
+int anos, salario;
+
+printf("a quantos anos tu trampa aqui?\n");
+scanf("%d", &anos);
+
+printf("qual é o seu salário?");
+scanf("%d", &salario);
+if(anos > 5){
+    salario = (salario / 100) * 120;
+    printf ("o seu salário atualizado é R$ %d", salario);
+} else {
+salario = (salario / 100) * 110;
+printf("o seu salário atualizado é R$ %d", salario);}
+
+}

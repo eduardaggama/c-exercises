cdfc82edb4397d55d34f918d09a9f45cd7ebfcef
Eduarda Gama <eduarda.gama@salg.com.br>
2018-09-03 10:47:15 -0300

    Create ex6-produto.cpp

---
+++ ex6-produto.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+
+int num;
+int num2;
+int resultado;
+
+printf("digite o primeiro número\n");
+scanf ("%d", &num);
+
+printf("digite o segundo número\n");
+scanf ("%d", &num2);
+
+resultado = num * num2;
+
+printf("o produto deles é %d", resultado);
+
+}
